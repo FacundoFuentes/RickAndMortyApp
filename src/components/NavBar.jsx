76899480e2f1ea21947b514d253bee70f5b7f69b
FacundoFuentes <facufuentes5335@gmail.com>
2021-09-09 22:05:04 -0300

    Not done, needed to add a few changes to the page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,69 +1,96 @@
-import React, {useState, useEffect} from 'react'
-import { Link, NavLink } from 'react-router-dom'
-import { Button } from './Button.jsx'
-import './styles/NavBar.css'
+import React, { useState, useEffect } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { Button } from "./Button.jsx";
+import "./styles/NavBar.css";
 
 const NavBar = () => {
-    const [click, setClick] = useState(false)
-    const [button, setButton] = useState(true)
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
-    const [colorChange, setColorChange] = useState(false)
+  const [colorChange, setColorChange] = useState(false);
 
-    function handleColorChange() {
-        if (window.scrollY >= 627) setColorChange(true)
-        else setColorChange(false)
-    }
+  function handleColorChange() {
+    if (window.scrollY >= 627) setColorChange(true);
+    else setColorChange(false);
+  }
 
-    window.addEventListener('scroll', handleColorChange)
+  window.addEventListener("scroll", handleColorChange);
 
-    const handleClick = () => setClick(!click)
-    const closeMobileMenu = () => setClick(false)
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
-    const showButton = () => {
-        if (window.innerWidth <= 960) setButton(false)
-        else setButton(true)
-    }
+  const showButton = () => {
+    if (window.innerWidth <= 960) setButton(false);
+    else setButton(true);
+  };
 
-    useEffect(() => {
-        showButton() 
-    }, [])
+  useEffect(() => {
+    showButton();
+  }, []);
 
-    window.addEventListener('resize', showButton)
+  window.addEventListener("resize", showButton);
 
-    return (
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <NavLink
+            to="/"
+            className={colorChange ? "navbar-logo-color2" : "navbar-logo"}
+            onClick={closeMobileMenu}
+          >
+            <i className={`fas fa-disease`}></i>
+          </NavLink>
+          <div className="menu-icon" onClick={handleClick}>
+            {colorChange ? (
+              <p className={"bars-color2"}>O</p>
+            ) : (
+              <p className={"bars"}>O</p>
+            )}
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link
+                to="/"
+                className={colorChange ? "nav-links-color2" : "nav-links"}
+                onClick={closeMobileMenu}
+              >
+                Inicio
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/about"
+                className={colorChange ? "nav-links-color2" : "nav-links"}
+                onClick={closeMobileMenu}
+              >
+                Sobre Mi
+              </Link>
+            </li>
+            <li>
+            <a
+              className={colorChange ? "nav-links-mobile-color2" : "nav-links-mobile"}
+              href="https://github.com/FacundoFuentes/RickAndMortyApp"
+              target="_blank"
+              onClick={closeMobileMenu}
+            >
+              <i class="fab fa-github"></i>
+            </a>
+            </li>
+          </ul>
+          {button && (
+            <a
+              className={colorChange ? "nav-links-color2" : "nav-links"}
+              href="https://github.com/FacundoFuentes/RickAndMortyApp"
+              target="_blank"
+            >
+              <i class="fab fa-github"></i>
+            </a>
+          )}
+        </div>
+      </nav>
+    </>
+  );
+};
 
-            <>
-                <nav className="navbar">
-                    <div className="navbar-container">
-                        <NavLink to='/' className={colorChange ? 'navbar-logo-color2' : 'navbar-logo'} onClick={closeMobileMenu}>
-                            <i className={`fas fa-disease`}></i>
-                        </NavLink>
-                        <div className="menu-icon" onClick={handleClick}>
-                            {colorChange ? (<p className={'bars-color2'}>O</p>) : (<p className={'bars'}>O</p>)}
-                            
-                        </div>
-                        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                            <li className='nav-item'>
-                                <Link to='/' className={colorChange ? 'nav-links-color2' : 'nav-links'} onClick={closeMobileMenu}>
-                                Inicio
-                                </Link>
-                            </li>
-                            <li className='nav-item'>
-                                <Link to='/about' className={colorChange ? 'nav-links-color2' : 'nav-links' } onClick={closeMobileMenu}>
-                                Sobre Mi
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to='/sign-up' className={colorChange ? 'nav-links-mobile-color2' : 'nav-links-mobile'} onClick={closeMobileMenu}>
-                                <i class="fab fa-github"></i>
-                                </Link>
-                            </li>
-                        </ul>
-                        {button && <Button buttonStyle={colorChange ? 'btn--outline-color2' : 'btn--outline'} buttonSize='btn--large' path={'/register'}><i class="fab fa-github"></i></Button>}
-                    </div>
-                </nav>
-            </>
-    )
-}
-
-export default NavBar
+export default NavBar;
